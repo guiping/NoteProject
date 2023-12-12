8c02d8c4b0b5ae2b1dd032cf9b1bb9ff6aa475c2
guiping <gping.vip@gmail.com>
2023-12-12 18:38:11 +0800

    增加签名信息

--- app/src/androidTest/java/com/example/myapplicationtest/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myapplicationtest/ExampleInstrumentedTest.kt
@@ -26,14 +26,22 @@ class ExampleInstrumentedTest {
     @Test
     fun test111(){
         val secretKey =   EncryptorUtils.generateSecretKey()
-        Log.e("pLog","secretKey--- ：secretKey=  $secretKey")
-       val url1 =  EncryptorUtils.encrypt("https://kofejwfigergd.top/wap.html",secretKey)
-        Log.e("pLog","加密信息--- ：url1=  $url1")
-       val url2=  EncryptorUtils.decrypt(url1,secretKey)
-        Log.e("pLog","解密信息--- ：  $url2")
-        val url3 =  EncryptorUtils.encrypt("https://landing-page.cdn-dysxb.com/8k8/",secretKey)
-        val url4 =  EncryptorUtils.encrypt("https://t.me/cskh_8k8",secretKey)
-        Log.e("pLog","加密信息--- ：url3 =   $url3")
-        Log.e("pLog","加密信息--- ：url4 =   $url4")
+        Log.e("pLog","secretKey--- ：secretKey=  ${EncryptorUtils.keyToString(secretKey) }   ")
+//       val url1 =  EncryptorUtils.encrypt("https://kofejwfigergd.top/wap.html",secretKey)
+//        Log.e("pLog","加密信息--- ：url1=  $url1")
+//       val url2=  EncryptorUtils.decrypt(url1,secretKey)
+//        Log.e("pLog","解密信息--- ：  $url2")
+//        val url3 =  EncryptorUtils.encrypt("https://landing-page.cdn-dysxb.com/8k8/",secretKey)
+//        val url4 =  EncryptorUtils.encrypt("https://t.me/cskh_8k8",secretKey)
+//        Log.e("pLog","加密信息--- ：url3 =   $url3")
+//        Log.e("pLog","加密信息--- ：url4 =   $url4")
+
+//        val url2=  EncryptorUtils.decrypt("bWhhgzllLXGxUl2eT1xQdYoWR6WRTgKnx4TE1dnZdX9MUYVrkG3oAiwtj/uWWLyp",EncryptorUtils.generateSecretKey("[B@f7ba3b0"))
+//        Log.e("pLog","加密信息--- ：url2 =   $url2")
+//        val url3=  EncryptorUtils.decrypt("bSSJ0IIxPnXtuy0VDEBF9dwwDxpCs7ShcP3xYZ1T1qK4ubLfRH3T2VvriA+qa8gE",EncryptorUtils.generateSecretKey("[B@f7ba3b0"))
+//        Log.e("pLog","加密信息--- ：url3 =   $url3")
+//        val url4=  EncryptorUtils.decrypt("8R+HmpijTP1qRCC0yqf7MavxfY3+X+GVuxH2kDvRCl4=",EncryptorUtils.generateSecretKey("[B@f7ba3b0"))
+//        Log.e("pLog","加密信息--- ：url4 =   $url4")
+
     }
 }
--- app/src/main/java/com/odfudndh/mvjsu/utils/EncryptorUtils.kt
+++ app/src/main/java/com/odfudndh/mvjsu/utils/EncryptorUtils.kt
@@ -1,33 +1,47 @@
 package com.odfudndh.mvjsu.utils
 
 
-import android.util.Base64
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.util.*
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
+@RequiresApi(Build.VERSION_CODES.O)
 object EncryptorUtils {
     private const val TRANSFORMATION = "AES/CBC/PKCS7Padding"
     private const val KEY_SIZE = 256
 
-    fun encrypt(text: String, secretKey: SecretKey): String {
-        val cipher = Cipher.getInstance(TRANSFORMATION)
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, IvParameterSpec( ByteArray(16)))
-        val encryptedBytes = cipher.doFinal(text.toByteArray())
-        return Base64.encodeToString(encryptedBytes, Base64.DEFAULT)
-    }
-
-    fun decrypt(encryptedText: String, secretKey: SecretKey): String {
-        val cipher = Cipher.getInstance(TRANSFORMATION)
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, IvParameterSpec( ByteArray(16)))
-        val decryptedBytes = cipher.doFinal(Base64.decode(encryptedText, Base64.DEFAULT))
-        return String(decryptedBytes)
-    }
-
-    fun generateSecretKey(): SecretKey {
+      fun generateSecretKey(): SecretKey {
         val keyGenerator = KeyGenerator.getInstance("AES")
-        keyGenerator.init(KEY_SIZE)
+        keyGenerator.init(128)
         return keyGenerator.generateKey()
+    }
+      fun keyToString(secretKey: SecretKey): String {
+        return Base64.getEncoder().encodeToString(secretKey.encoded)
+    }
+
+      fun stringToKey(stringKey: String): SecretKey {
+        val keyBytes = Base64.getDecoder().decode(stringKey)
+        return SecretKeySpec(keyBytes, "AES")
+    }
+
+      fun encryptAES(data: String, stringKey: String): String {
+        val secretKey = stringToKey(stringKey)
+        val cipher = Cipher.getInstance("AES")
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+        val encryptedBytes = cipher.doFinal(data.toByteArray())
+        return Base64.getEncoder().encodeToString(encryptedBytes)
+    }
+
+      fun decryptAES(encryptedData: String, stringKey: String): String {
+        val secretKey = stringToKey(stringKey)
+        val cipher = Cipher.getInstance("AES")
+        cipher.init(Cipher.DECRYPT_MODE, secretKey)
+        val decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedData))
+        return String(decryptedBytes)
     }
 }
